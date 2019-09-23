0baf569c601d97d7d9fad42842cc2d9195a38ee8
没懂 <kuaichuan888@163.com>
2019-09-23 13:59:13 +0800

    动态图标

--- echarts01/src/App.vue
+++ echarts01/src/App.vue
@@ -1,7 +1,10 @@
 <template>
   <div id="app">
     <div id="echartContainer" style="width: 500px; height: 500px;"></div>
+
+    <div id="dynamicChart"  style="width: 500px; height: 500px;"></div>
   </div>
+
 </template>
 
 <script>
@@ -9,10 +12,20 @@
 export default {
   name: 'app',
   data() {
-    return {}
+    return {
+      timer:"",
+      // 随机数
+      value: Math.random() * 1000,
+      now :new Date(1997, 9, 3),
+      oneDay :24 * 3600 * 1000,
+      // 时间数组
+      data:[],
+      myChart:''
+    }
   },
 
   methods: {
+    // 创建 图标
     getChart() {
       // 1. 获取表格容器
       var myChart = echarts.init(document.getElementById("echartContainer"));
@@ -61,9 +74,105 @@ export default {
 
       });
     },
+
+    // 随机数据
+    randomData() {
+      this.now = new Date(+this.now + this.oneDay);
+      this.value = this.value + Math.random() * 21 - 10;
+      return {
+        name: this.now.toString(),
+        value: [
+          [this.now.getFullYear(), this.now.getMonth() + 1, this.now.getDate()].join('/'),
+          Math.round(this.value)
+        ]
+      }
+    },
+    /* 初始化数据 */
+    initData(){
+      for (var i = 0; i < 1000; i++) {
+        this.data.push(this.randomData());
+      }
+    },
+
+    cheateDynamicChart(){
+      // 1. 获取表格容器
+      this.myChart = echarts.init(document.getElementById("dynamicChart"));
+      // 2. 设置图表组件值
+      this.myChart.setOption({
+        title: {
+          text: '动态数据 + 时间坐标轴'
+        },
+        tooltip: {
+          trigger: 'axis',
+          formatter: function (params) {
+            params = params[0];
+            var date = new Date(params.name);
+            return date.getDate() + '/' + (date.getMonth() + 1) + '/' + date.getFullYear() + ' : ' + params.value[1];
+          },
+          axisPointer: {
+            animation: false
+          }
+        },
+        xAxis: {
+          type: 'time',
+          splitLine: {
+            show: false
+          }
+        },
+        yAxis: {
+          type: 'value',
+          boundaryGap: [0, '100%'],
+          splitLine: {
+            show: false
+          }
+        },
+        series: [{
+          name: '模拟数据',
+          type: 'line',
+          showSymbol: false,
+          hoverAnimation: false,
+          data: this.data
+        }]
+      });
+    },
+
+    timelog()
+    {
+      // this.value++;
+      // console.log(this.value);
+      this.showDate()
+    },
+    showDate(){
+      this.now = new Date(+this.now + this.oneDay);
+      // 日期每秒
+      // console.log(this.now)
+    },
+    // 更新数据
+    updateData(){
+      for (var i = 0; i < 5; i++) {
+        this.data.shift();
+        this.data.push(this.randomData());
+      }
+      this.myChart.setOption({
+        series: [{
+          data: this.data
+        }]
+      });
+    }
   },
   mounted(){
-    this.getChart()
+    this.getChart();
+    // 1. 生成图标随机数据
+    this.initData();
+
+    // 2. 创建图表
+    this.cheateDynamicChart();
+    // 3. 刷新数据
+    this.timer = setInterval(this.updateData,1000);
+  },
+  beforeDestroy()
+  {
+    clearInterval(this.timer)
   }
 }
 
@@ -79,21 +188,4 @@ export default {
   margin-top: 60px;
 }
 
-h1, h2 {
-  font-weight: normal;
-}
-
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-
-a {
-  color: #42b983;
-}
 </style>
