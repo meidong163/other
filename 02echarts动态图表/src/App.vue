8aaf19e403f1939adebed4ea8b45fab79338fbb5
没懂 <kuaichuan888@163.com>
2019-09-23 16:21:55 +0800

    vue 路由

--- 02echarts动态图表/src/App.vue
+++ 02echarts动态图表/src/App.vue
@@ -1,7 +1,5 @@
 <template>
   <div id="app">
-    <div id="echartContainer" style="width: 500px; height: 500px;"></div>
-
     <div id="dynamicChart"  style="width: 500px; height: 500px;"></div>
   </div>
 
@@ -25,55 +23,6 @@ export default {
   },
 
   methods: {
-    // 创建 图标
-    getChart() {
-      // 1. 获取表格容器
-      var myChart = echarts.init(document.getElementById("echartContainer"));
-      // 2. 设置各组件样式
-      myChart.setOption({
-        //表头
-        title: {text: '柱状图表'},
-        // 工具箱
-        toolbox: {
-          show: true,
-          feature: {
-            magicType: {show: true, type: ['stack', 'tiled']},
-            saveAsImage: {show: true}
-          }
-        },
-        // x轴
-        xAxis: {
-          type: 'category',
-          boundaryGap: false,
-          data: ['周一', '周二', '周三', '周四', '周五', '周六', '周日']
-        },
-        // y轴
-        yAxis: {
-          type: 'value'
-        },
-
-        // 展示数据
-        series: [{
-          name: '成交',
-          type: 'line',
-          smooth: true,
-          data: [10, 12, 21, 54, 260, 830, 710]
-        },
-          {
-            name: '预购',
-            type: 'line',
-            smooth: true,
-            data: [30, 182, 434, 791, 390, 30, 10]
-          },
-          {
-            name: '意向',
-            type: 'line',
-            smooth: true,
-            data: [1320, 1132, 601, 234, 120, 90, 20]
-          }]
-
-      });
-    },
 
     // 随机数据
     randomData() {
@@ -135,18 +84,6 @@ export default {
         }]
       });
     },
-
-    timelog()
-    {
-      // this.value++;
-      // console.log(this.value);
-      this.showDate()
-    },
-    showDate(){
-      this.now = new Date(+this.now + this.oneDay);
-      // 日期每秒
-      // console.log(this.now)
-    },
     // 更新数据
     updateData(){
       for (var i = 0; i < 5; i++) {
@@ -161,10 +98,8 @@ export default {
     }
   },
   mounted(){
-    this.getChart();
     // 1. 生成图标随机数据
     this.initData();
-
     // 2. 创建图表
     this.cheateDynamicChart();
     // 3. 刷新数据
--- 02echarts动态图表/src/main.js
+++ 02echarts动态图表/src/main.js
@@ -4,4 +4,5 @@ import App from './App.vue'
 new Vue({
   el: '#app',
   render: h => h(App)
-})
+//  配置路由
+});
