77f8092f42977a3fca8a05d8dd10b1e73eb94c02
没懂 <kuaichuan888@163.com>
2019-09-25 19:49:11 +0800

    Props down, events up

---
+++ VUE基础/父子组件传值/demo01/src/demo/demo.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="container">
+    <h3 @click="upClick">{{transMsg}}</h3>
+    <!--<input type="button" value="click" @click="upClick">-->
+  </div>
+</template>
+
+<script>
+    export default {
+        name: "demo",
+      data(){
+          return{
+
+          }
+      },
+      methods:{
+          upClick(){
+            this.$emit('func',"好好学习，天天向上")
+          }
+      }
+      ,
+      props:['transMsg']
+    }
+</script>
+
+<style scoped>
+
+</style>
